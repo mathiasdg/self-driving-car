49a573b87aa519db466d6f2285a0cd134b1ac0e5
mathiasdg <mathias.de.gucht@gmail.com>
2025-11-21 16:08:38 +0100

    intersect util added

--- src/Helpers/utils.js
+++ src/Helpers/utils.js
@@ -11,3 +11,13 @@ export function drawCurrentCenter() {
 	strokeWeight(69)
 	point(0,0)
 }
+
+export function getIntersection(A, B, C, D) {
+	/*
+		Ix = Ax + (Bx - Ax) * (Cy - Ay) / (Dy - Cy)
+		Iy = Ay + (By - Ay) * (Cy - Ay) / (Dy - Cy)
+	*/
+	let Ix = A.x + (B.x - A.x) * (C.y - A.y) / (D.y - C.y)
+	let Iy = A.y + (B.y - A.y) * (C.y - A.y) / (D.y - C.y)
+	return {x: Ix, y: Iy}
+}
